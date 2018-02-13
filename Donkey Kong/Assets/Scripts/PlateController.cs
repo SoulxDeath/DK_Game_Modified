9c3dfcd36e02f7f057d54629ef9e3a046a9b8149
SoulxDeath <HFairbanks@users.noreply.github.com>
2018-02-13 09:01:19 -0500

    Update

---
+++ Donkey Kong/Assets/Scripts/PlateController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateController : MonoBehaviour {
+
+
+
+	public float speed = 5f;
+
+
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		transform.Translate (speed * Time.deltaTime, 0, 0);
+	}
+}
---
+++ Donkey Kong/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+
+	public float jump;
+
+	public float moveVelocity;
+
+
+	public GameObject ladder;
+
+	void Start()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+
+		if (Input.GetKey ("w")) 
+		{
+			moveUp ();
+		}
+
+		if(Input.GetKey ("s"))
+		{
+			moveDown ();
+		}
+
+		if(Input.GetKey ("a"))
+		{
+			moveLeft ();
+		}
+
+		if(Input.GetKey ("d"))
+		{
+			moveRight ();
+		}
+
+		//Jumping
+		if (Input.GetKey ("z")) 
+		{
+			jumpPlayer ();
+		}
+	}
+
+	void moveUp()
+	{
+		transform.position += new Vector3 (0, moveSpeed);
+	}
+	void moveDown()
+	{
+		transform.position -= new Vector3 (0, moveSpeed);
+	}
+	void moveLeft()
+	{
+		transform.position -= new Vector3 (moveSpeed, 0);
+	}
+	void moveRight()
+	{
+		transform.position += new Vector3 (moveSpeed, 0);
+	}
+	void jumpPlayer()
+	{
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jump);
+	}
+
+}
